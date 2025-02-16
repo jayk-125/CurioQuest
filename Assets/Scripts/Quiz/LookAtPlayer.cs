a69a6f029b397490f71ce1861cbada9295a29093
jayk-125 <leejaykin@gmail.com>
2025-02-16 23:15:39 +0800

    Multi-choice is now good to go
    -Added prefabs and IDs for each answer for all questions
    -Added Script with a label that follows camera

---
+++ Assets/Scripts/Quiz/LookAtPlayer.cs
@@ -0,0 +1,26 @@
+/*
+* Author: Loh Shau Ern Shaun
+* Date: 20/07/2024
+* Description: 
+* Makes canvas UI face player camera at all times
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Late Update is called once per frame, AFTER Update function
+    void LateUpdate()
+    {
+        Transform cam = GameObject.Find("/NestedParentArmature_Unpack/MainCamera").transform;
+        // Face canvas object towards the player
+        transform.LookAt(cam);
+    }
+}
